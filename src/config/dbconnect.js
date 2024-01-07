81832d2128d4fc0c5b5637beb6eca1d8685fc479
Caio Lene <80287851+CaioLCM@users.noreply.github.com>
2024-01-07 09:16:15 -0300

    Atualizar o dbconnect.js

--- src/config/dbconnect.js
+++ src/config/dbconnect.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose"
 
-async function conectar(){
-    mongoose.connect("mongodb+srv://caiolene:admin123@cluster0.tv8laoy.mongodb.net/Livrario?retryWrites=true&w=majority")
+    mongoose.connect("URL")
     return mongoose.connection
 }
 
